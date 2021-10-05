1f89c09663d7f25c77585f6ab4f8eda5417eb7ec
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-10-05 17:36:00 +0200

    Ex 3

--- session1/src/main/kotlin/Main.kt
+++ session1/src/main/kotlin/Main.kt
@@ -1,3 +1,37 @@
 fun main() {
-    println("Hello World!");
+    val options = arrayOf("Rock", "Paper", "Scissor", "Lizard", "Spock")
+    val computerChoice = options.random()
+    val userChoice = getUserChoice(options)
+
+    println("Computer picked $computerChoice")
+    println("User picked $userChoice")
+    determineWinner(computerChoice, userChoice)
+
+    fun getUserChoice(options: Array<String>): String? {
+        var userChoice: String?
+        do {
+            println("Make your choice: rock - paper - scissor")
+            userChoice = readLine()
+        } while(!options.contains(userChoice)) {
+            return userChoice
+        }
+    }
+
+    fun determineWinner(computerChoice: String, userChoice: String) {
+        if(computerChoice === userChoice) {
+            println("It's a draw")
+            return
+        }
+    }
+
+    val winMessage = "You've won!"
+    val loseMessage = "You've lost!"
+
+    when(computerChoice) {
+        "rock" -> if (userChoice == "paper") println(winMessage) else println(loseMessage)
+        "paper" -> if (userChoice == "scissors") println(winMessage) else println(loseMessage)
+        "scissors" -> if (userChoice == "rock") println(winMessage) else println(loseMessage)
+
+    }
 }
+
