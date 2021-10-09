39bcff1ebee15d033c859c90914be5aed5b5810f
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-10-09 20:05:24 +0200

    Rewrote some code, fixed bugs

--- session1/src/main/kotlin/Main.kt
+++ session1/src/main/kotlin/Main.kt
@@ -6,31 +6,32 @@ fun main() {
     println("Computer picked $computerChoice")
     println("User picked $userChoice")
     determineWinner(computerChoice, userChoice)
+}
 
-    fun getUserChoice(options: Array<String>): String? {
-        var userChoice: String?
-        do {
-            println("Make your choice: rock - paper - scissor")
-            userChoice = readLine()
-        } while(!options.contains(userChoice)) {
-            return userChoice
-        }
-    }
+fun getUserChoice(options: Array<String>): String? {
+    var userChoice: String?
+    do {
+        println("Make your choice: Rock - Paper - Scissors - Lizard - Spock")
+        userChoice = readLine();
+    } while (!options.contains(userChoice))
+        return userChoice
+}
 
-    fun determineWinner(computerChoice: String, userChoice: String) {
-        if(computerChoice === userChoice) {
-            println("It's a draw")
-            return
-        }
+fun determineWinner(computerChoice: String, userChoice: String?) {
+    if(computerChoice == userChoice) {
+        println("It's a draw")
+        return
     }
 
     val winMessage = "You've won!"
     val loseMessage = "You've lost!"
 
     when(computerChoice) {
-        "rock" -> if (userChoice == "paper") println(winMessage) else println(loseMessage)
-        "paper" -> if (userChoice == "scissors") println(winMessage) else println(loseMessage)
-        "scissors" -> if (userChoice == "rock") println(winMessage) else println(loseMessage)
+        "rock" -> if (userChoice == "paper" || userChoice == "spock") println(winMessage) else println(loseMessage)
+        "paper" -> if (userChoice == "scissors" || userChoice == "lizard") println(winMessage) else println(loseMessage)
+        "scissors" -> if (userChoice == "rock" || userChoice == "spock") println(winMessage) else println(loseMessage)
+        "lizard" -> if (userChoice == "rock" || userChoice == "scissor") println(winMessage) else println(loseMessage)
+        "spock" -> if (userChoice == "lizard" || userChoice == "paper") println(winMessage) else println(loseMessage)
 
     }
 }
